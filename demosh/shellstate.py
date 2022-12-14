7bacc96c407ebfb930a6c7c5632ef768637b4bbb
Flynn <flynn@buoyant.io>
2022-12-14 18:22:06 -0500

    Don't run an interactive shell for cd and assignments
    Signed-off-by: Flynn <flynn@buoyant.io>

--- demosh/shellstate.py
+++ demosh/shellstate.py
@@ -181,7 +181,7 @@ class ShellState:
         return 0
 
     def do_cd(self, demostate: 'DemoState', cmd: str) -> int:
-        proc = subprocess.Popen(["bash", "-i"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        proc = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 cwd=self.cwd, env=self.env, close_fds=True)
 
         assert proc.stdin is not None   # hush, mypy
@@ -211,7 +211,7 @@ class ShellState:
 
         # print("assign '%s' = '%s'" % (name, value))
 
-        proc = subprocess.Popen(["bash", "-i"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        proc = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 cwd=self.cwd, env=self.env, close_fds=True)
 
         assert proc.stdin is not None   # hush, mypy
